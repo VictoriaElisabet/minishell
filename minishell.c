cb794a58ebffc447674b1809bda889464cc03fcc
vgrankul <vgrankul@debian.com>
2020-06-22 16:55:31 +0300

    added read prompt funct and .h file

--- minishell.c
+++ minishell.c
@@ -1,9 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   filearr.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vgrankul <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/22 11:28:19 by vgrankul          #+#    #+#             */
+/*   Updated: 2019/10/31 13:49:20 by vgrankul         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include <stdio.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include "minishell.h"
 
-int main(void)
+void    read_prompt(char *prompt)
 {
-    printf("%d \n %d", getpid(), getppid());
+    int ret;
+    char buf[BUF_SIZE + 1];
+
+    ft_printf("%s", prompt);
+    while ((ret = read(0, buf, BUF_SIZE)) > 0)
+    {
+        if(ft_strchr(buf, EOF) || ft_strchr(buf, '\n'))
+            break ;
+    }
+}
+
+int main()
+{
+    while(1)
+        read_prompt("Enter info: ");
+    //int fd;
+    //fd = open(stdin, O_RDONLY);
+
+    //printf("%d \n %d", getpid(), getppid());
 }
---
+++ minishell.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vgrankul <vgrankul@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/18 10:24:05 by vgrankul          #+#    #+#             */
+/*   Updated: 2020/02/27 13:01:07 by vgrankul         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <string.h>
+#include "./libft/libft.h"
+
+# define BUF_SIZE 32
+
+#endif
