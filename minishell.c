f554ac7155299855509f52022c723a2caeef8168
vgrankul <vgrankul@debian.com>
2020-06-24 12:49:00 +0300

    read_prompt funct now read one char at a time

--- minishell.c
+++ minishell.c
@@ -18,9 +18,9 @@
 #include <fcntl.h>
 #include "minishell.h"
 
-void    read_prompt(char *prompt)
+char    *read_prompt(char *prompt)
 {
-    int ret;
+    /*int ret;
     char buf[BUF_SIZE + 1];
 
     ft_printf("%s", prompt);
@@ -28,15 +28,41 @@ void    read_prompt(char *prompt)
     {
         if(ft_strchr(buf, EOF) || ft_strchr(buf, '\n'))
             break ;
+    }*/
+    int     ret;
+    char     ch[2];
+    char    *prompt_string;
+    char    *tmp;
+
+    prompt_string = NULL;    
+    ft_printf("%s", prompt);
+    while((ret = read(0, &ch, 1)) > 0)
+    {
+        ch[ret] = '\0';
+        if ((int)ch[0] == EOF || ch[0] == '\n')
+        {
+
+            break ;
+        }
+        if (prompt_string == NULL)
+            prompt_string = ft_strnew(0);
+        tmp = ft_strjoin(prompt_string, ch);
+        free(prompt_string);
+        prompt_string = tmp;
     }
+    return (prompt_string);
 }
 
 int main()
 {
+    char *prompt_string;
     while(1)
-        read_prompt("Enter info: ");
+    {
+        prompt_string = read_prompt("Enter info: ");
+        ft_printf("strin = %s\n", prompt_string);
+    }
     //int fd;
     //fd = open(stdin, O_RDONLY);
-
+    
     //printf("%d \n %d", getpid(), getppid());
 }
