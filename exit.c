9fed58c0219fabc2b7bb632f08dd35a14a43c4f8
vgrankul <vgrankul@debian.com>
2020-07-25 20:40:06 +0300

    Added exit as a new funct

---
+++ exit.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_exit.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vgrankul <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/22 11:28:19 by vgrankul          #+#    #+#             */
+/*   Updated: 2019/10/31 13:49:20 by vgrankul         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+int    ft_exit(int argc, char **argv, int status)
+{
+    if (argc > 2)
+        ft_printf("%s: too many arguments\n", argv[0]);
+    else if (argc < 2)
+        exit(status);
+    else
+        exit(ft_atoi(argv[1]));
+    return (0);
+}
